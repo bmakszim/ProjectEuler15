27ba99e1b73c4a7c7c599dbc9383c7ca84fb3b04
Balázs Makszim <b.makszim@hotmail.com>
2020-09-23 22:11:22 +0200

    Very slow solution

---
+++ Algorithm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler15
+{
+    class Algorithm
+    {
+        private const int size = 20;
+        public double Counter { get; set; } = 0;
+
+        public double Run()
+        {
+            VerrrrySlowRun(new Position { X = 1, Y = 0 });
+            return Counter * 2;
+        }
+
+        public void VerrrrySlowRun(Position pos)
+        {
+            if (pos.X == size && pos.Y == size)
+            {
+                Counter++;
+                return;
+            }
+
+            if (pos.X > size || pos.Y > size)
+            {
+                return;
+            }
+
+            VerrrrySlowRun(MoveRight(pos));
+            VerrrrySlowRun(MoveDown(pos));
+        }
+
+        private Position MoveRight(Position pos)
+        {
+            pos.X++;
+            return pos;
+        }
+
+        private Position MoveDown(Position pos)
+        {
+            pos.Y++;
+            return pos;
+        }
+    }
+}
